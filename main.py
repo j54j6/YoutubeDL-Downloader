692d90a09a1786d2e15ea66cf04bbaffee956f44
Justin Ritter@j54j6 <justin-ritter@outlook.com>
2024-04-12 23:33:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 
 #
 # Project by j54j6
-# This program is used to make a private copy of youtube videos and potentially other websites supported by youtubedl
-# Furthermore it supports automatic periodic checking of channels and auto downloading. 
-# It also checks if the downlaoded video already exists in the specified storage space and checks integrity of videos and redownload them if needed
-# 
+# This program is used to make a private copy of youtube videos and potentially other websites
+# supported by youtubedl Furthermore it supports automatic periodic checking
+# of channels and auto downloading.
+# It also checks if the downlaoded video already exists in the specified storage space and checks
+# integrity of videos and redownload them if needed
+#
 
 
 #
-# This file contains the "User Logic" this means that all User Experience related stuff is located in here and also the "main Controls"
+# This file contains the "User Logic" this means that all User Experience related stuff is located
+# in here and also the "main Controls"
 # In general this projects is made out of different modules (see ReadME ). This file combines them...
 #
 
@@ -34,5 +37,5 @@ def main():
         exit()
     
     #Check for database
-    
+
 
